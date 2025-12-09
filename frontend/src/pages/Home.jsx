36284cc804a379ecafc4fa0d6c27cc060133fae0
Mariesoso <inezakellia12345@gmail.com>
2025-12-09 14:21:34 +0100

    new change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,8 @@ import Navbar from '../components/Navbar'
 import HowItWorks from '../components/HowItWorks'
 import BlogSection from '../components/Blog'
 import Testimonial from '../components/Testimonial'
+import Herosection from '../components/Herosection'
+import Aboutsection from '../components/Aboutsection'
 
 const Home = () => {
     return (
