a3ec99adffd6b149934664637380344e3711bb3a
Fabrice-Dush <dushimimanafabricerwanda@gmail.com>
2025-12-09 11:16:14 +0100

    Added frontend code

--- frontend/src/routers/index.jsx
+++ frontend/src/routers/index.jsx
@@ -11,6 +11,7 @@ import DepartmentDashboard from "../pages/dashboard/DepartmentPage";
 import EmployeeManagementDashboard from "../pages/dashboard/EmployeeManagement";
 import TradeManagementSystem from "../pages/dashboard/employee/trade/TradeManagementSystem";
 import StudentDashboard from "../pages/dashboard/StudentPage";
+import ClassManagementDashboard from "../pages/dashboard/class/ClassManagement";
 
 const LoadingSpinner = () => (
   <div className="loading-spinner">
@@ -26,7 +27,7 @@ const SuspenseWrapper = ({ children }) => {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout></MainLayout>,
+    element: <MainLayout />,
     children: [{ index: true, element: <Home /> }],
   },
   {
@@ -39,13 +40,13 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Navigate to={"/employee/dashboard"}></Navigate>,
+        element: <Navigate to="/employee/dashboard" replace />,
       },
       {
         path: "dashboard",
         element: (
           <SuspenseWrapper>
-            <EmployeeDashboardLayout role={"employee"} />{" "}
+            <EmployeeDashboardLayout role="employee" />
           </SuspenseWrapper>
         ),
         children: [
@@ -55,6 +56,10 @@ const router = createBrowserRouter([
           { path: "employees", element: <EmployeeManagementDashboard /> },
           { path: "trades", element: <TradeManagementSystem /> },
           { path: "students", element: <StudentDashboard /> },
+          {
+            path: "classes",
+            element: <ClassManagementDashboard />,
+          },
         ],
       },
     ],
@@ -63,6 +68,10 @@ const router = createBrowserRouter([
     path: "/auth",
     element: <Outlet />,
     children: [{ path: "employee/login", element: <EmployeeLogin /> }],
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" replace />,
   },
 ]);
 
