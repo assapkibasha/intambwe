5b5e48c9d11cf3106e53fc1611dd784c46860a9e
Mutabazi Judson <mutabazijudson@gmail.com>
2025-12-09 13:29:31 +0200

    New Changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import HowItWorks from '../components/HowItWorks'
+import BlogSection from '../components/Blog'
 
 const Home = () => {
     return (
-        <div>
-            <Navbar />
-            Home
+        <div className='min-h-screen'>
+            <HowItWorks />
+            <BlogSection />
+
         </div>
     )
 }
