2e5149b3b42f3c8922e0cbc316b1bd9667723dfd
Mariesoso <inezakellia12345@gmail.com>
2025-12-09 12:28:59 +0100

    pulling alice changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
+
 import ServiceSection from '../components/ServiseSection';
+import Navbar from '../components/Navbar'
 import Testimonial from '../components/Testimonial'
-import Herosection from '../components/Herosection';
-import Aboutsection from '../components/Aboutsection';
 
 
 const Home = () => {
     return (
         <div>
             <Navbar />
-            <Herosection/>
-            <Aboutsection/>
 
             <ServiceSection/>
 
